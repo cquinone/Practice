bd4b7ada7e8c8e5760d6c2f3fca169dd0c2d39be
klawal <rckgoodvibes@gmail.com>
2017-03-16 20:13:49 -0400

    Update CTCI91.java

--- CTCI91.java
+++ CTCI91.java
@@ -4,7 +4,6 @@ public class CTCI91 {
 	public static void main(String args[]) {
 		System.out.println(CTCI91(10));
 	}
-	
 	public static int CTCI91(int n) {
 		/*
 		 * A child is running up a staircase with n steps, and can hop 
@@ -14,18 +13,11 @@ public class CTCI91 {
 		 */
 		return stephelper(n,0,0);
 	}
-	
 	public static int stephelper(int n,int step,int inc) {
 		int newstep = step + inc;
-
-		if (newstep == n)
-			return 1;
-		else if (newstep > n)
-			return 0;
+		if (newstep == n) return 1;
+		else if (newstep > n) return 0;
 		else
-			return stephelper(n,newstep,1) + 
-					stephelper(n,newstep,2) +
-					stephelper(n,newstep,3);
-		
+			return stephelper(n,newstep,1) + stephelper(n,newstep,2) + stephelper(n,newstep,3);	
 	}
 }
